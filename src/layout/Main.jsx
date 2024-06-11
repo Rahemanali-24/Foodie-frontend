44953fab76a182babd24c95e28fc78eec9e3e5ec
Rahemanali-24 <159017790+Rahemanali-24@users.noreply.github.com>
2024-06-11 14:26:17 +0530

    Added Navbar component

---
+++ src/layout/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+
+export const Main = () => {
+  return (
+    <div>
+        <Navbar/>
+
+        <Outlet/>
+
+        <footer>Footer</footer>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        "green": "#39DB4A",
+        "red": "#FF6868",
+        "secondary": "#555",
+        "primaryBG": "#FCFCFC"
+      },
+      
+    },
+  },
+  plugins: [
+    daisyui,
+  ],
+}
+
